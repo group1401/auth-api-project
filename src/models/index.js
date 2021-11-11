48585c9b0252be64bf9385f08ccc25c397aa9594
HaninHaidrah <haneenhadey@gmail.com>
2021-11-11 08:23:28 +0200

    option

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const Collection = require('../models/data-collection');
 
 const { Sequelize, DataTypes } = require('sequelize');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory';
 
 
 let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
